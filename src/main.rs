5cbfa2b23806a380890aa39d7044a75277adea76
Greg Zometa <greg.so.meta@gmail.com>
2023-11-07 22:34:06 -0800

    first app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use leptos::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button on:click=move |_| {
+            set_count.update(|n| *n += 4);
+        }>            "Click me: "
+        {count}</button>
+    }
+}
+
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(|| view! { <App /> })
 }
