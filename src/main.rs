0629aaec7c76866d0dd23753fedc1feaae7aea4d
Greg Zometa <greg.so.meta@gmail.com>
2023-11-07 22:43:21 -0800

    dynamic classes

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-        <button on:click=move |_| {
-            set_count.update(|n| *n += 4);
-        }>            "Click me: "
-        {count}</button>
+        <button
+        class=("red", move || count() > 0 && count() % 2 == 1)
+        class=("blue", move || count() > 0 && count() % 2 == 0)
+        on:click=move |_| {
+            set_count.update(|n| *n += 1);
+        }>
+          "Click me: "{count}
+        </button>
     }
 }
 
